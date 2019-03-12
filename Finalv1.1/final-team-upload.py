a18823cc0447d04c2235c9087534a2d209e38af5
TeamA56 <45287449+TeamA56@users.noreply.github.com>
2019-03-12 13:22:31 +0000

    Add files via upload

--- Finalv1.1/final-team-upload.py
+++ Finalv1.1/final-team-upload.py
@@ -9,26 +9,26 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
-# Read the train data. this data is used to train our model.
-training_data = pd.read_csv('C:\\Users\\natha\\OneDrive - University of Essex\\Team Project\\home-data-for-ml-course\\train.csv')#change file directory to test
+# Read the data
+train = pd.read_csv('train.csv')
 
-# we are creating the model
-train_y = training_data.SalePrice
-predictor_vals = ['LotArea', 'OverallQual', 'YearBuilt', 'TotRmsAbvGrd']#these are the variables we have selected to use.
-#these variables will be the ones the model focuses on when we predict the test data.
+# pull data into target (y) and predictors (X)
+train_y = train.SalePrice
+predictor_cols = ['LotArea', 'OverallQual', 'YearBuilt', 'TotRmsAbvGrd']
+
 # Create training predictors data
-train_X = training_data[predictor_vals]
+train_X = train[predictor_cols]
 
-The_model = RandomForestRegressor()#the type of regression used
-The_model.fit(train_X, train_y)
+my_model = RandomForestRegressor()
+my_model.fit(train_X, train_y)
 # Read the test data
-test_data_read = pd.read_csv('C:\\Users\\natha\\OneDrive - University of Essex\\Team Project\\home-data-for-ml-course\\test.csv')
+test = pd.read_csv('test.csv')
 # Treat the test data in the same way as training data. In this case, pull same columns.
-test_X = test_data_read[predictor_vals]
+test_X = test[predictor_cols]
 # Use the model to make predictions
-predicted_prices = The_model.predict(test_X)
+predicted_prices = my_model.predict(test_X)
 # We will look at the predicted prices to ensure we have something sensible.
 print(predicted_prices)
-my_submission = pd.DataFrame({'Id': test_data_read.Id, 'SalePrice': predicted_prices})
-#We have elected to keep with file name submission (any name can be chosen).
+my_submission = pd.DataFrame({'Id': test.Id, 'SalePrice': predicted_prices})
+# you could use any filename. We choose submission here
 my_submission.to_csv('submission.csv', index=False)
